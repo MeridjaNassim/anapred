e237fe1ee3e4d15e87eba3c20d0574ba05768cac
MeridjaNassim <ha_meridja@esi.dz>
2020-06-28 11:25:29 +0100

    Modified gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,9 +17,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-nprogress`,
       options: {
-        // Setting a color is optional.
-        color: `#212121`,
-        // Disable the loading spinner.
+        color: `#1174EF`,
         showSpinner: true,
       },
     },
